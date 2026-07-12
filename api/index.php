1e2a72bc9963d24bfe17937c04c18c35c84decb5
Zacky595 <zackyraffa595@gmail.com>
2026-07-12 13:26:46 +0700

    buat shadow folder dan bersihkan file lokal

--- api/index.php
+++ api/index.php
@@ -1,3 +1,25 @@
 <?php
 
+// 1. Buat folder bayangan di direktori /tmp (satu-satunya folder yang tidak dikunci Vercel)
+$tmpDir = '/tmp/laravel';
+$directories = [
+    "$tmpDir/storage/logs",
+    "$tmpDir/storage/framework/views",
+    "$tmpDir/storage/framework/cache",
+    "$tmpDir/storage/framework/cache/data",
+    "$tmpDir/storage/framework/sessions",
+    "$tmpDir/bootstrap/cache",
+];
+
+foreach ($directories as $dir) {
+    if (! is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
+// 2. Paksa Laravel menggunakan folder bayangan tersebut secara otomatis
+putenv("VIEW_COMPILED_PATH=$tmpDir/storage/framework/views");
+$_ENV['VIEW_COMPILED_PATH'] = "$tmpDir/storage/framework/views";
+
+// 3. Jalankan aplikasi Laravel
 require __DIR__.'/../public/index.php';
